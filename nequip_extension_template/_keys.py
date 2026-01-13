0a33bc315970400713bb48b819640273a47783ba
cw-tan <chuinwei.tan@gmail.com>
2026-01-13 09:52:51 +0800

    fix swapped field registration

--- nequip_extension_template/_keys.py
+++ nequip_extension_template/_keys.py
@@ -7,6 +7,6 @@ DEV_FACING_NODE_FIELD_KEY: Final[str] = "user_facing_node_field_name"
 
 # register fields so that NequIP's data processing utilities can process the new keys
 register_fields(
-    graph_fields=[DEV_FACING_NODE_FIELD_KEY],
-    node_fields=[DEV_FACING_GRAPH_FIELD_KEY],
+    graph_fields=[DEV_FACING_GRAPH_FIELD_KEY],
+    node_fields=[DEV_FACING_NODE_FIELD_KEY],
 )
